8c0aeeb3f4ae4b24864872744c82ae4e1c251e0b
Jorge Quintero <83592930+JorgeQuinteroAyala@users.noreply.github.com>
2026-04-30 23:36:08 -0600

    Delete Exodus.Agent.Core directory

--- Exodus.Agent.Core/Services/IVerificadorPlataforma.cs
+++
@@ -1,8 +0,0 @@
-namespace Exodus.Agent.Core.Services;
-
-public interface IVerificadorPlataforma
-{
-    string NombrePlataforma { get; }
-
-    Task<bool> EstaAlcanzableAsync(CancellationToken pCancelacionToken);
-}
