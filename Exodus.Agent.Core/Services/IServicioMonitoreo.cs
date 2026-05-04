1a8b9c94e31660b696a0852291de3aee40532a1b
Jorge Quintero <83592930+JorgeQuinteroAyala@users.noreply.github.com>
2026-05-04 10:03:15 -0600

    Delete Exodus.Agent.Core directory

--- Exodus.Agent.Core/Services/IServicioMonitoreo.cs
+++
@@ -1,8 +0,0 @@
-namespace Exodus.Agent.Core.Services;
-
-public interface IServicioMonitoreo
-{
-    string NombrePlataforma { get; }
-
-    Task EjecutarUnaVezAsync(CancellationToken pCancelacionToken);
-}
